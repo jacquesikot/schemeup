964f3cfb60cd33ba33ad7bccd102be5ebf51f5aa
Godwin Ekhueorohan <snr.sangodzi@gmail.com>
2023-05-28 15:43:31 +0100

    added delete modal

--- src/images/icons/TrashIconPlain.tsx
+++ src/images/icons/TrashIconPlain.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { SVGProps } from 'react';
+
 const TrashIconPlain = (props: SVGProps<SVGSVGElement>) => (
   <svg xmlns="http://www.w3.org/2000/svg" width={16} height={16} fill="none" {...props}>
     <path
