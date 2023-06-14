c7f9a7c33a7a50e4441ed6dd36c3b5d69c194a6e
mento-mume <akpankevman@gmail.com>
2023-06-14 12:13:03 +0100

    first commit

--- client/src/components/canvas/SchemaProperties.tsx
+++ client/src/components/canvas/SchemaProperties.tsx
@@ -1,15 +1,23 @@
-import { Typography, Box, IconButton, TextField, styled, Checkbox, Grid } from '@mui/material';
-import { useTheme } from '@mui/material/styles';
+import {
+  Typography,
+  Box,
+  IconButton,
+  TextField,
+  styled,
+  Checkbox,
+  Grid,
+} from "@mui/material";
+import { useTheme } from "@mui/material/styles";
 
-import SideBarToggleOpen from '../../images/icons/SideBarToggleOpen';
-import Switch from '../Switch';
-import { useEffect, useState } from 'react';
-import Button from '../global/Button';
-import { useParams } from 'react-router-dom';
-import { useAppDispatch, useAppSelector } from '../../redux/hooks';
-import { updateSchema } from '../../redux/slice/schemas';
-import AiSuggestCard from './AiSuggestCard';
-import TableV2 from './TableV2';
+import SideBarToggleOpen from "../../images/icons/SideBarToggleOpen";
+import Switch from "../Switch";
+import { useEffect, useState } from "react";
+import Button from "../global/Button";
+import { useParams } from "react-router-dom";
+import { useAppDispatch, useAppSelector } from "../../redux/hooks";
+import { updateSchema } from "../../redux/slice/schemas";
+import AiSuggestCard from "./AiSuggestCard";
+import TableV2 from "./TableV2";
 
 interface SchemaPropertiesProps {
   toggleOpen: (open: boolean) => void;
@@ -20,26 +28,26 @@ interface SchemaPropertiesProps {
 }
 
 const StyledTextField = styled(TextField)({
-  '& label.Mui-focused': {
-    color: '#A0AAB4',
+  "& label.Mui-focused": {
+    color: "#A0AAB4",
   },
-  '& .MuiInput-underline:after': {
-    borderBottomColor: '#B2BAC2',
+  "& .MuiInput-underline:after": {
+    borderBottomColor: "#B2BAC2",
   },
-  '& .MuiOutlinedInput-root': {
-    color: '#667085',
+  "& .MuiOutlinedInput-root": {
+    color: "#667085",
     fontSize: 16,
     height: 126,
 
-    '& fieldset': {
-      borderColor: '#E0E3E7',
+    "& fieldset": {
+      borderColor: "#E0E3E7",
       borderRadius: 8,
     },
-    '&:hover fieldset': {
-      borderColor: '#B2BAC2',
+    "&:hover fieldset": {
+      borderColor: "#B2BAC2",
     },
-    '&.Mui-focused fieldset': {
-      borderColor: '#6941C6',
+    "&.Mui-focused fieldset": {
+      borderColor: "#6941C6",
     },
   },
 });
@@ -55,33 +63,52 @@ const SchemaProperties = ({
   const colors = theme.palette;
   const params = useParams();
   const schema = useAppSelector(
-    (state) => state.schemas.schemas.filter((s) => s.id === params.id)[0] || ({ name: '' } as any)
+    (state) =>
+      state.schemas.schemas.filter((s) => s.id === params.id)[0] ||
+      ({ name: "" } as any)
   );
   const activeTableArray =
-    schema.activeTable && schema.tables ? schema.tables.filter((t) => t.id === schema.activeTable) : [];
+    schema.activeTable && schema.tables
+      ? schema.tables.filter((t) => t.id === schema.activeTable)
+      : [];
   const activeTable = activeTableArray.length > 0 ? activeTableArray[0] : null;
-  const activeTableName = activeTableArray.length > 0 ? activeTableArray[0].name : '';
+  const activeTableName =
+    activeTableArray.length > 0 ? activeTableArray[0].name : "";
   const dispatch = useAppDispatch();
-  const [value, setValue] = useState('Settings');
+  const [value, setValue] = useState("Settings");
   const [description, setDescription] = useState(schema.description);
   const [activeSuggestions, setActiveSuggestions] = useState(suggestions);
 
   useEffect(() => {
     if (activeTableName) {
-      setActiveSuggestions(suggestions.filter((s) => s.body.toLowerCase().includes(activeTableName.toLowerCase())));
+      setActiveSuggestions(
+        suggestions.filter((s) =>
+          s.body.toLowerCase().includes(activeTableName.toLowerCase())
+        )
+      );
     } else {
       setActiveSuggestions(suggestions);
     }
   }, [suggestions, activeTableName]);
 
   return (
-    <Box pl={2} pr={2} width={'100%'} pt={2}>
-      <Box display={'flex'} justifyContent={'space-between'} alignItems={'center'} mb={3}>
+    <Box pl={2} pr={2} width={"100%"} pt={2}>
+      <Box
+        display={"flex"}
+        justifyContent={"space-between"}
+        alignItems={"center"}
+        mb={3}
+      >
         <Box>
           <Typography fontSize={18} fontWeight={600} color={colors.grey[900]}>
             Schema
           </Typography>
-          <Typography fontSize={14} fontWeight={400} color={colors.grey[700]} mt={1}>
+          <Typography
+            fontSize={14}
+            fontWeight={400}
+            color={colors.grey[700]}
+            mt={1}
+          >
             Get more from your schema
           </Typography>
         </Box>
@@ -91,31 +118,36 @@ const SchemaProperties = ({
         </IconButton>
       </Box>
 
-      <Switch value={value} setValue={setValue} badgeValue={suggestions.length} />
+      <Switch
+        value={value}
+        setValue={setValue}
+        badgeValue={suggestions.length}
+      />
 
-      {value === 'Settings' ? (
+      {value === "Settings" ? (
         schema.activeTable && activeTable ? (
           <Grid
             container
-            maxHeight={'75vh'}
+            maxHeight={"75vh"}
             pb={2}
-            display={'flex'}
-            justifyContent={'center'}
+            display={"flex"}
+            justifyContent={"center"}
             sx={{
-              overflowY: 'auto' /* To allow main grid scroll vertically but not entire screen */,
-              scrollbarWidth: 'none' /* Firefox */,
-              '-ms-overflow-style': 'none' /* IE 10+ */,
-              '&::-webkit-scrollbar': {
+              overflowY:
+                "auto" /* To allow main grid scroll vertically but not entire screen */,
+              scrollbarWidth: "none" /* Firefox */,
+              "-ms-overflow-style": "none" /* IE 10+ */,
+              "&::-webkit-scrollbar": {
                 width: 0,
                 height: 0,
               },
-              '&::-webkit-scrollbar-track': {
-                background: 'transparent',
+              "&::-webkit-scrollbar-track": {
+                background: "transparent",
               },
-              '&::-webkit-scrollbar-thumb': {
-                background: 'transparent',
+              "&::-webkit-scrollbar-thumb": {
+                background: "transparent",
               },
-              '-webkit-overflow-scrolling': 'touch' /* For hide on scroll */,
+              "-webkit-overflow-scrolling": "touch" /* For hide on scroll */,
             }}
           >
             <Grid>
@@ -130,14 +162,17 @@ const SchemaProperties = ({
                         name: c.name,
                         type: c.type,
                         primaryKey: c.primaryKey,
-                        foreignKey: activeTable.foreignKeys.filter((fk: any) => fk.column === c.name).length > 0,
+                        foreignKey:
+                          activeTable.foreignKeys.filter(
+                            (fk: any) => fk.column === c.name
+                          ).length > 0,
                         nullable: c.nullable,
                         index: c.index,
                         autoInc: c.autoInc,
                         autoUpdateTime: c.autoUpdateTime,
-                        comment: '',
+                        comment: "",
                         unique: c.unique,
-                        default: '',
+                        default: "",
                       };
                     }),
                   }}
@@ -148,18 +183,30 @@ const SchemaProperties = ({
         ) : (
           <>
             <Box>
-              <Typography fontSize={14} fontWeight={500} color={colors.grey[900]} mt={3} mb={1}>
+              <Typography
+                fontSize={14}
+                fontWeight={500}
+                color={colors.grey[900]}
+                mt={3}
+                mb={1}
+              >
                 Description
               </Typography>
 
               <StyledTextField
                 multiline
-                style={{ width: '100%' }}
+                style={{ width: "100%" }}
                 value={description}
                 onChange={(e) => setDescription(e.target.value)}
               />
 
-              <Box display={'flex'} width={'100%'} justifyContent={'flex-end'} mb={5} mt={2}>
+              <Box
+                display={"flex"}
+                width={"100%"}
+                justifyContent={"flex-end"}
+                mb={5}
+                mt={2}
+              >
                 <Button
                   type="primary"
                   label="Save"
@@ -174,7 +221,7 @@ const SchemaProperties = ({
                 />
               </Box>
 
-              <Box display={'flex'} alignItems={'center'}>
+              <Box display={"flex"} alignItems={"center"}>
                 <Checkbox
                   checked={!schema.meta.showColumns || false}
                   onChange={(e) =>
@@ -189,14 +236,27 @@ const SchemaProperties = ({
                     )
                   }
                 />
-                <Typography variant="body2" fontWeight={500} fontSize={14} color={colors.grey[600]}>
+                <Typography
+                  variant="body2"
+                  fontWeight={500}
+                  fontSize={14}
+                  color={colors.grey[600]}
+                >
                   Hide Table Columns
                 </Typography>
               </Box>
 
-              <Box display={'flex'} alignItems={'center'}>
-                <Checkbox checked={!showRelations} onChange={(e) => toggleRelations(!showRelations)} />
-                <Typography variant="body2" fontWeight={500} fontSize={14} color={colors.grey[600]}>
+              <Box display={"flex"} alignItems={"center"}>
+                <Checkbox
+                  checked={!showRelations}
+                  onChange={(e) => toggleRelations(!showRelations)}
+                />
+                <Typography
+                  variant="body2"
+                  fontWeight={500}
+                  fontSize={14}
+                  color={colors.grey[600]}
+                >
                   Hide Relations
                 </Typography>
               </Box>
@@ -205,34 +265,44 @@ const SchemaProperties = ({
         )
       ) : (
         <>
-          <Typography mt={2}>{schema.activeTable ? `Filtering for ${activeTableName}` : 'Entire Schema'}</Typography>
+          <Typography mt={2}>
+            {schema.activeTable
+              ? `Filtering for ${activeTableName}`
+              : "Entire Schema"}
+          </Typography>
           <Grid
             container
-            maxHeight={'71vh'}
+            maxHeight={"71vh"}
             pt={2}
             pb={2}
             sx={{
-              overflowY: 'auto' /* To allow main grid scroll vertically but not entire screen */,
-              scrollbarWidth: 'none' /* Firefox */,
-              '-ms-overflow-style': 'none' /* IE 10+ */,
-              '&::-webkit-scrollbar': {
+              overflowY:
+                "auto" /* To allow main grid scroll vertically but not entire screen */,
+              scrollbarWidth: "none" /* Firefox */,
+              "-ms-overflow-style": "none" /* IE 10+ */,
+              "&::-webkit-scrollbar": {
                 width: 0,
                 height: 0,
               },
-              '&::-webkit-scrollbar-track': {
-                background: 'transparent',
+              "&::-webkit-scrollbar-track": {
+                background: "transparent",
               },
-              '&::-webkit-scrollbar-thumb': {
-                background: 'transparent',
+              "&::-webkit-scrollbar-thumb": {
+                background: "transparent",
               },
-              '-webkit-overflow-scrolling': 'touch' /* For hide on scroll */,
+              "-webkit-overflow-scrolling": "touch" /* For hide on scroll */,
             }}
           >
             <Grid>
-              <Box display={'flex'} flexDirection={'column'} alignItems={'center'}>
-                {activeSuggestions &&
-                  activeSuggestions.length > 0 &&
-                  activeSuggestions.map((s, index) => <AiSuggestCard key={index} title={s.title} body={s.body} />)}
+              <Box
+                display={"flex"}
+                flexDirection={"column"}
+                alignItems={"center"}
+              >
+                <AiSuggestCard
+                  title="title"
+                  body="bodydddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd"
+                />
               </Box>
             </Grid>
           </Grid>
