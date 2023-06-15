5ba07f79bad746b184e546d5d23be6ff6328dff7
Jacques Ikot <jacquesikot@gmail.com>
2023-06-15 17:53:53 +0100

    base vercel serverless set up + fix dev startup

--- server/api/handler.ts
+++ server/api/handler.ts
@@ -2,7 +2,7 @@ import type { VercelRequest, VercelResponse } from '@vercel/node';
 
 export default function handler(request: VercelRequest, response: VercelResponse) {
   response.status(200).json({
-    body: 'Test Response',
+    body: 'Test Response latest',
     query: request.query,
     cookies: request.cookies,
   });
